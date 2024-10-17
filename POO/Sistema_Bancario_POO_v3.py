1f141fe0159869284e49898e6cfdbd7d3e381eb0
IgorDomingos <idrs@cin.ufpe.br>
2024-10-17 01:40:55 -0300

    feat: add Sistema_Bancario_V3 POO

---
+++ POO/Sistema_Bancario_POO_v3.py
@@ -0,0 +1,111 @@
+#Sistema Bancario_Versão_3_POO
+from abc import ABC, abstractclassmethod
+import textwrap
+
+class Cliente:
+    def __init__(self, endereco):
+        self.endereco = endereco
+        self.contas = []
+    
+    def realizar_transacao(self, conta, transacao):
+        transacao.registrar(conta)
+
+    def adicionar_conta(self, conta):
+        self.contas.append(conta)
+    
+class PessoaFisica(Cliente):
+    def __init__(self, cpf, nome, data_nascimento, endereco):
+        super().__init__(endereco)
+        self.cpf = cpf
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+
+class Conta:
+    def __init__(self, numero, cliente):
+        self._saldo = 0
+        self._numero = numero
+        self._agencia = "0001"
+        self._cliente = cliente
+        self._historico = Historico()
+
+    @classmethod
+    def nova_conta(cls, cliente, numero):
+        return cls(numero, cliente)
+    
+    @property
+    def numero(self):
+        return self._numero
+    
+    @property
+    def agencia(self):
+        return self._agencia
+    
+    @property
+    def cliente(self):
+        return self._cliente
+
+    @property
+    def saldo(self):
+        return self._saldo
+    
+    @property
+    def historico(self):
+        return self._historico
+    
+    def sacar(self, valor):
+        self.valor = valor
+
+    def depositar(self, valor):
+        self.valor = valor
+
+class ContaCorrente(Conta):
+    def __init__(self, saldo, numero, agencia, cliente, historico, limite, limite_saques):
+        super().__init__(saldo, numero, agencia, cliente, historico)
+        self.limite = limite
+        self.limite_saques = limite_saques
+    
+class Historico:
+    def __init__(self):
+        pass
+
+    def adicionar_transacao(self, transacao):
+        self.transacao = transacao
+    
+class Transacao(ABC):
+    @property
+    @abstractclassmethod
+    def valor(self):
+        pass
+
+    @abstractclassmethod
+    def registrar(self, conta):
+        pass
+
+class Deposito(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+    
+    @property
+    def valor(self):
+        return self._valor
+
+    def registrar(self, conta):
+        sucesso_transacao = conta.depositar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
+
+class Saque(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+    
+    @property
+    def valor(self):
+        return self._valor
+
+    def registrar(self, conta):
+        sucesso_transacao = conta.sacar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
+        
